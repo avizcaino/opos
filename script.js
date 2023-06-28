9b464a1869e722c08137b9ca62bb60db106ea117
Alex Vizcaino <avizcainoluna@gmail.com>
2023-06-28 06:21:31 +0200

    fix: update styling

--- script.js
+++ script.js
@@ -1,58 +1,70 @@
 // Default theme
 import "@splidejs/splide/css";
-
 // or other themes
 import "@splidejs/splide/css/skyblue";
 // import "@splidejs/splide/css/sea-green";
-
 // or only core styles
-import "@splidejs/splide/css/core";
-
 import Splide from "@splidejs/splide";
-new Splide(".splide").mount({}, MyTransition);
-
+import "@splidejs/splide/css/core";
 import { EventInterface } from "@splidejs/splide";
 
-export function MyTransition(Splide, Components) {
-  const { bind } = EventInterface(Splide);
-  const { Move } = Components;
-  const { list } = Components.Elements;
+document.addEventListener("DOMContentLoaded", function () {
+  const main = new Splide("#main-carousel", {
+    type: "fade",
+    rewind: true,
+    pagination: false,
+    arrows: false,
+  });
 
-  let endCallback;
+  const thumbnails = new Splide("#thumbnail-carousel", {
+    fixedWidth: 100,
+    fixedHeight: 60,
+    gap: 5,
+    rewind: true,
+    pagination: false,
+    isNavigation: true,
+    breakpoints: {
+      300: {
+        fixedWidth: 60,
+        fixedHeight: 44,
+      },
+    },
+  });
 
-  function mount() {
-    bind(list, "transitionend", (e) => {
-      if (e.target === list && endCallback) {
-        // Removes the transition property
-        cancel();
+  const bar = main.root.querySelector(".my-carousel-progress-bar");
 
-        // Calls the `done` callback
-        endCallback();
-      }
-    });
-  }
+  // Updates the bar width whenever the carousel moves:
+  main.on("mounted move", function () {
+    var end = main.Components.Controller.getEnd() + 1;
+    var rate = Math.min((main.index + 1) / end, 1);
+    bar.style.width = String(100 * rate) + "%";
+  });
 
-  function start(index, done) {
-    // Converts the index to the position
-    const destination = Move.toPosition(index, true);
+  main.sync(thumbnails);
+  main.mount();
+  thumbnails.mount();
+});
 
-    // Applies the CSS transition
-    list.style.transition = "transform 800ms cubic-bezier(.44,.65,.07,1.01)";
+function enlargeImage(ev) {
+  console.log(ev.currentTarget);
+  const dialog = document.querySelector("dialog");
+  const dialogImg = dialog.querySelector("img");
+  dialogImg.src = ev.currentTarget.src;
+  const dialogOverlay = document.createElement("div");
+  dialogOverlay.classList.add("dialogOverlay");
+  document.body.appendChild(dialogOverlay);
+  dialogOverlay.style.display = "block";
 
-    // Moves the carousel to the destination.
-    Move.translate(destination);
+  dialog.addEventListener("click", function (event) {
+    if (event.target !== dialogImg) {
+      dialog.close();
+      dialogOverlay.style.display = "none"; // Hide the overlay
+    }
+  });
 
-    // Keeps the callback to invoke later.
-    endCallback = done;
-  }
-
-  function cancel() {
-    list.style.transition = "";
-  }
-
-  return {
-    mount,
-    start,
-    cancel,
-  };
+  dialog.showModal();
 }
+
+Array.from(
+  document.querySelector("#main-carousel").querySelectorAll(".splide__slide")
+)?.map((element) => element.addEventListener("click", enlargeImage));
